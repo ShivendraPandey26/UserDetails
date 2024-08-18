6072865a1d94230c7e1736cbd18119861c78e640
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-08-19 01:26:55 +0530

    Create App.jsx

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+import CreateUser from './components/CreateUser';
+import Read from './components/Read';
+import Update from './components/Update';
+
+
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <main className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<CreateUser />} />
+          <Route path="/read" element={<Read />} />
+          <Route path="/:name" element={<Update />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
