7cbcb2e2f4313960ecff22756525de8897fd1e2b
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-08-21 00:53:29 +0530

    Update Update.jsx

--- Frontend/src/components/Update.jsx
+++ Frontend/src/components/Update.jsx
@@ -225,7 +225,7 @@ const UserForm = () => {
             type="submit"
             className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            Submit
+            Update
           </button>
         </div>
       </form>
