6f14f5a9fac24d16b2f1e915796bc15d5d50bb57
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-08-22 00:09:46 +0530

    Create UserModel.js

---
+++ Backend/models/UserModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+// create a Schema for user
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Create a model for User
+const UserModel = mongoose.model("UserModel", userSchema);
+
+export default UserModel;
