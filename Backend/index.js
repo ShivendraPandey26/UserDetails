96d771a1d554bbaf361dd65f39dc7fdf4032ca0b
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-08-26 17:52:56 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,41 +5,19 @@ config();
 import cors from "cors";
 import router from "./routes/userDataRoute.js";
 import { userRouter } from "./routes/userRoute.js";
-import passport from "passport";
-import { Strategy as LocalStrategy } from "passport-local";
-import UserModel from "./models/UserModel.js";
+import passport from "./Auth/LocalAuth.js";
 
 const app = express();
 
 // Middleware for parsing JSON request bodies
 app.use(express.json());
 app.use(cors());
-app.use(passport.initialize());
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      console.log("Received request", username, password);
-      // find the user by username
-      const user = await UserModel.findOne({ userName: username });
-      if (!user) {
-        return done(null, false, { message: "Invalid userName" });
-      }
-      // check if the password is correct
-      const isPasswordMatch = (user.password) === password ? true : false;
-      if (!isPasswordMatch) {
-        return done(null, false, { message: "Invalid password" });
-      }
-      // if everything is ok, return the user
-      return done(null, user);
-    } catch (error) {
-      return done(null, false, { message: error.message });
-    }
-  })
-);
 
+app.use(passport.initialize());
 const localauth = passport.authenticate('local', {session: false}) ;
+
 // Routes
-app.use("/",localauth ,router);
+app.use("/" ,router);
 app.use("/users/user", userRouter);
 
 const startServer = async () => {
@@ -56,6 +34,4 @@ const startServer = async () => {
     process.exit(1);
   }
 };
-
-// Initialize the server
 startServer();
